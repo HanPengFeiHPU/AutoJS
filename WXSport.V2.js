40f1382fa2488e56feb93fa3cb2d01fbeeaccb4d
HanPengfei <1060003755@qq.com>
2022-07-26 13:34:33 +0800

    依据坐标点赞，显示终端

---
+++ WXSport.V2.js
@@ -0,0 +1,64 @@
+var app = '微信';
+var temp = launchApp(app);
+var ignorePerson = ['张耀方', '童鑫麟', '陈夕珩','黎祥'];
+var flag = 0;
+
+console.show();
+function dz() {  //点赞功能
+
+    var member = id("com.tencent.mm:id/cmb").find();    //获取控件的全称ID
+    var names = id("com.tencent.mm:id/cmp").find();
+
+    console.log("当前人员数量：",member.length);
+    for (var i = 1; i < member.length; i++) {          //遍历集合元素
+
+        var index = ignorePerson.indexOf(names.get(i).text());
+
+        if (index == -1) { // 在跳过人员的名单中
+
+            var temp = member.get(i).click();       //选择元素进行点击
+
+            // var temp = click(member[i].bounds().centerX(),member[i].bounds().centerY());       //依据坐标点赞
+
+            console.log("当前人员：",names.get(i).text(),temp);
+            ignorePerson[ignorePerson.length] = names.get(i).text(); // 加入跳过人员名单中
+
+            sleep(600);
+        }
+
+        if (text("赞我的朋友").findOnce()) {        // 判断是否点到自己如果是则返回
+
+            back();                               //功能键返回
+            sleep(700);
+        }
+
+        if (i == member.length - 1) {
+            if (scrollDown()) {                           //遍历完完成后进行翻页操作
+                sleep(200);
+            }
+            else {
+                flag = 1;
+                toast("点赞完成");
+            }
+        }
+    }
+}
+
+function main() {
+    while (flag == 0) {
+        if (temp) {
+            sleep(1000);
+            click("微信运动");
+            sleep(1000);
+            click('步数排行榜');
+            sleep(1000);
+            dz();
+        }
+        else {
+            toast("程序出错");
+        }
+    }
+
+    toast("程序已退出");
+}
+main();
