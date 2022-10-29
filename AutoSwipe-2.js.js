a14e5945b99055abc5f544759d819a8336241718
HanPengfei <1060003755@qq.com>
2022-10-29 19:20:26 +0800

    添加清理内存功能，避免假死

--- AutoSwipe-2.js.js
+++ AutoSwipe-2.js.js
@@ -34,14 +34,63 @@ function run()
         toast("以观看个数："+i+"\n剩余个数："+(total-i))
     }
 }
+
+function clean()
+{
+    var appName2 = "手机管家";
+    launchApp(appName2);
+    sleep(3000);
+    
+    if(text("不同意").exists()){
+        while(!text("不同意").click());
+        sleep(1000);
+    }
+
+    if(text("取消").exists()){
+        text("取消").click();
+        sleep(1000);
+    }
+
+    if(text("垃圾清理").exists()){
+        while(!click("垃圾清理")){
+            toast("点击垃圾清理");
+        };
+        sleep(1000);
+    }
+
+    // 停止清理按钮
+    while(id("button_stop").exists()){
+        sleep(3000);
+    }
+
+    // 清理按钮
+    if(id("button_clean").exists()){
+        id("button_clean").click();
+        sleep(5000);
+    }
+
+    while(!id("icon_folder_creation_bg").exists()){
+        back();
+        if(text("取消").exists()){
+            text("取消").click();
+            sleep(1000);
+        }
+    }
+}
+
 function main()
 {
-    var cycleNumber=100000;
-    for(var j=0;j<cycleNumber;j++){
+    var i = 0;
+    while(true){
         var appName = "快手极速版";
         launchApp(appName);
         sleep(3000);
         run();
+        i = i + 1;
+        // if(i % 100 == 0){
+        //     clean();
+        // }
+        clean();
     }
 }
 main()
